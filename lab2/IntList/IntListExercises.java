7c4b8d611623da6937daab60fcea0e733dbeeba0
chen-cyber <2239984205.qq.com>
2025-09-02 22:40:13 +0800

    complete lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,11 +10,11 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
-        head.first += c;
+;
     }
 
     /**
@@ -27,7 +27,7 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if ( (p.first == max(p) && firstDigitEqualsLastDigit(max(p))) || p.first < 10) {
+            if ( (p.first == max(p) && firstDigitEqualsLastDigit(max(p))) || p.first / 10 == 0) {
                 p.first = 0;
             }
 
